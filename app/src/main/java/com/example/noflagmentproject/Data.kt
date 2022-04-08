c090274c30669dfd47944b8ea5738d4e5c8500db
meta_shogo.h <shogo.honma@metamoji.com>
2022-04-08 16:58:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/noflagmentproject/Data.kt
@@ -0,0 +1,4 @@
+package com.example.noflagmentproject
+
+class Data {
+}
---
+++ app/src/main/java/com/example/noflagmentproject/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.noflagmentproject
+
+import android.os.Bundle
+import android.widget.CheckBox
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity() {
+
+    // ViewModelのインスタンス作成、またはインスタンス取得 コードの理解度低い
+    private val viewModel: CheckedViewModel by lazy {
+        ViewModelProvider(
+            this, ViewModelProvider.NewInstanceFactory()
+        ).get(CheckedViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        println("-- onCreate --")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // シェアードインスタンスを生成
+        val data = Data.getInstance()
+
+        val checkBox1 = findViewById<CheckBox>(R.id.checkbox_1)
+        val checkBox2 = findViewById<CheckBox>(R.id.checkbox_2)
+
+        //シェアードインスタンス
+        if(data!=null){
+            checkBox1.isChecked = data.cb1_checked //true
+            checkBox2.isChecked = data.cb2_checked //false
+        }
+
+        //ViewModel
+        checkBox1.isChecked = viewModel.checkbox1 //false
+        checkBox2.isChecked = viewModel.checkbox2 //true
+
+    }
+
+
+
+
+}
